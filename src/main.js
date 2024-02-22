753c8c8ce1f239a7f1117afc1418d43e6a443b32
VictorDavo <victorda746@gmail.com>
2024-02-22 01:12:54 +0100

    push

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "40px",
+  duration: 1200,
+  delay: 100,
+  //reset: true;
+});
+
+sr.reveal(".scroll-reveal", { interval: 50, origin: "bottom" });
+
+const expBtn = document.getElementById("experiencia");
+const eduBtn = document.getElementById("educacion");
+
+const expTxt = document.getElementById("experienciaTxt");
+const eduTxt = document.getElementById("educacionTxt");
+
+function toggleVisibility(showElement, hideElement) {
+  showElement.classList.remove("hidden");
+  showElement.classList.add("grid");
+
+  hideElement.classList.remove("grid");
+  hideElement.classList.add("hidden");
+}
+
+function updateButtonStyles(activeBtn, inactiveBtn) {
+  activeBtn.classList.remove("text-grey");
+  activeBtn.classList.add("text-titlewhite");
+
+  inactiveBtn.classList.remove("text-titlewhite");
+  inactiveBtn.classList.add("text-grey");
+}
+
+expBtn.addEventListener("click", function () {
+  toggleVisibility(expTxt, eduTxt);
+  updateButtonStyles(expBtn, eduBtn);
+});
+
+eduBtn.addEventListener("click", function () {
+  toggleVisibility(eduTxt, expTxt);
+  updateButtonStyles(eduBtn, expBtn);
+});
